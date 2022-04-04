826f5637b35009683c65d20b1bfb289d117cf76d
Eddie1991-hub <54647669+Eddie1991-hub@users.noreply.github.com>
2022-04-04 22:11:11 +0200

    commit changes 04.04.2022

--- my-app-new/src/App.js
+++ my-app-new/src/App.js
@@ -1,4 +1,4 @@
-import Expenses from "./components/Expenses";
+import Expenses from "./components/Expenses/Expenses";
 
 function App() {
   const expenses = [
@@ -28,7 +28,7 @@ function App() {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      {/* <h2>Let's get started!</h2> */}
       <Expenses expenses={expenses} />
     </div>
   );
